fe1e0061839e7fce060840e8e8e8d6ac00c0abae
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-07-13 12:49:00 +0300

    Worked with the better-blog project which we did at the weekend

--- script.js
+++ script.js
@@ -4,53 +4,116 @@ console.log('it works');
 // Grab all the element that I might need for this exercise.
 
 const postList = document.querySelector("#post-list");
-// const cardDiv = document.querySelector(".card");
 const cardBody = document.querySelector(".card-body");
 const form = document.querySelector("#post-form");
-const newPostTitle = document.querySelector(`[aria-describedby="postTitle"]`);
+const newPostTitle = document.querySelector('#postTitle');
 const newPostContent = document.querySelector(`[name="postContent"]`);
 const newAuthor = document.querySelector(`[name="postAuthor"]`);
-console.log(newAuthor)
 const newImage = document.querySelector(".card-img-top");
-const submitButton = document.querySelector(".btn", "btn-primary");
-const hideButton = document.querySelector("#show-form");
-const errMessage = document.querySelector(".invalid-feedback hidden");
-
-
-    hideButton.addEventListener("click", () => {
-    const formCard = document.querySelector("#form-card");
-    formCard.hidden = "true";
-    hideButton.textContent = "Add a post";
-});
+const submitButton = document.querySelector(".btn-primary");
+const formCard = document.querySelector("#form-card");
+const showForm = document.querySelector("#show-form");
+const errMessage = document.querySelector(".invalid-feedback");
+const textAreaErrMsg = document.querySelector("#error-message");
+const deleteButton = document.querySelector(".btn-delete");
 
 
 
-// Add an event listener to the submit button.
-submitButton.addEventListener("click", ($event) => {
-    $event.preventDefault();
 
-    const myHTML = `
-<div class="card">
-    <img class="card-img-top" src="${newImage.value}">
-    <div class="card-body">
-        <h5 class="card-title">${newPostTitle.value}<small>${newAuthor.value}</small></h5>
-        <p class="card-title">${newPostContent.value}</p>
-        <input type="date" value="2020-07-11">
-        <button type="button" class="btn btn-sm btn-light btn-delete">Delete</button>
-    </div>
-</div>
-`;
-// postList.insertAdjacentHTML(`beforebegin`, myHTML);
+const newPost = ($event) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-const fragment = document.createRange().createContextualFragment(myHTML);
-postList.prepend(fragment)
-form.reset();
-if (newPostContent.value < 20) {
-    newPostContent.classList.add("is-invalid");
-    errMessage.removeClass();
+    const image = document.createElement('img');
+    image.classList.add('card-img-top');
+    image.src = 'https://picsum.photos/500/200';
+    image.alt ='Card image cap';
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const newTitle = document.createElement('h5');
+    newTitle.classList.add('card-title');
+
+    const author = document.createElement('small');
+    author.textContent = `${newAuthor.value}`;
+
+    const newContent = document.createElement('p');
+    newContent.classList.add('card-text');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn', 'btn-sm', 'btn-light', 'btn-delete');
+    deleteButton.textContent = 'Delete entry';
+
+    const date = document.createElement('div');
+    date.classList.add('card-footer', 'text-muted');
+
+    newTitle.appendChild(author);
+    cardBody.appendChild(newTitle);
+    cardBody.appendChild(newContent);
+    cardBody.appendChild(deleteButton);
+    card.appendChild(image);
+    card.appendChild(cardBody);
+    card.appendChild(date);
+    postList.prepend(card);
+
+    newTitle.textContent = `${newPostTitle.value}`;
+    author.textContent = `${newAuthor.value}`;
+    newContent.textContent = `${newPostContent.value}`;
 }
 
+showForm.addEventListener("click", () => {
+    formCard.classList.add("hidden");
+    showForm.textContent = "Add a post";
 });
+
+showForm.addEventListener('mousedown', () => {
+    formCard.classList.remove("hidden");
+    showForm.textContent = "Hide form";
+});
+
+
+
+const handleSubmit = ($event) => {
+    $event.preventDefault();
+    const form = $event.target;
+    
+    const postContent = form.postContent;
+    const nmbOfWords = postContent.value.split(" ").length;
+
+    if (nmbOfWords < 20) {
+        postContent.classList.add("is-invalid");
+        textAreaErrMsg.classList.remove("hidden");
+    } else {
+        const anotherPost = newPost($event);
+        postList.insertAdjacentElement('afterbegin', anotherPost);
+
+        postContent.classList.remove("is-invalid");
+        textAreaErrMsg.classList.add("hidden");
+    }
+
+    form.reset();
+
+};
+
+form.addEventListener('submit', handleSubmit);
+
+
+// A function for the delete button.
+const handleDelete = ($event) => {
+    if ($event.target.classList.contains('btn-delete')) {
+        const deleteBtn = $event.target;
+        deleteBtn.closest('.card').remove();
+    }
+};
+
+document.addEventListener('click', handleDelete);
+
+
+
+
+
+
 
 
 
